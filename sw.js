c9a533da05cf6720e042f900af6f114c6db8e1b3
Ramon Rojas <rojas9987cb12@gmail.com>
2022-04-11 18:14:48 -0500

    Modificamos el SW

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@
 importScripts('js/sw-utils.js');
 
 
-const STATIC_CACHE = 'static-v4';
-const DYNAMIC_CACHE = 'dynamic-v2';
+const STATIC_CACHE = 'static-v1';
+const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
